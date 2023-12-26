b6b01fc752ac19b25b463a6558cab02038dbcd29
Bandit Silachai <mbandit001@yahoo.co.th>
2023-12-26 08:58:18 +0700

    ✨ feature(app): json file

--- api/index.go
+++ api/index.go
@@ -7,4 +7,5 @@ import (
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	fmt.Fprintf(w, "<a href=\"/api/json\">/api/json</a>")
 }
---
+++ api/json.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func Json(w http.ResponseWriter, r *http.Request) {
+	responseData := map[string]string{
+		"message": "Hello, JSON!",
+	}
+
+	jsonData, err := json.Marshal(responseData)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
+}
